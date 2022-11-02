8cc0a6ec75c92105c1322c51a25efd3128567e21
Tom Nuzzarello <tjnuzza@gmail.com>
2022-11-02 15:12:05 -0500

    Added hourly wage calculation spreadsheet script

---
+++ Individual-scripts/Wage-calculations.py
@@ -0,0 +1,6 @@
+#Hourly wage calculation
+print('Hourly wages and monthly income')
+print('hours\t| 9/hr\t| 12/hr\t|15/hr')
+for i in range(1, 41):
+    #Hours times 4 weeks
+    print(i, '\t|', 36*i, '\t|', 48*i, '\t|', 60*i)
